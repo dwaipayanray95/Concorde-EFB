fbfa0893b067147c5dcc45f9c081b449eb1f704c
dwaipayanray95 <dwaipayanray95@gmail.com>
2025-12-25 18:07:02 +0530

    make sure app is building via .tsx and not older .js

--- src/App.js
+++ src/App.js
@@ -2,3 +2,4 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import ConcordeEFB from "./ConcordeEFB";
 const Root = () => _jsx(ConcordeEFB, {});
 export default Root;
+export { default } from "./App.tsx";
