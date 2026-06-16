c804528d2f0633e16d50c27427cadb98f52f4ef7
dwaipayanray95 <dwaipayanray95@gmail.com>
2026-06-16 19:25:05 +0530

    feat: set default launch window size and cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 import 'core/ui_tokens.dart';
 import 'screens/home_screen.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize window manager for Desktop platforms
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1300, 900), // Defined size to fit all widgets comfortably
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+    title: 'Concorde EFB',
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(
     const ProviderScope(
       child: ConcordeEfbApp(),
