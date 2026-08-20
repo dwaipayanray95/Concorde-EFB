dfbcadf40de9f02143712dd0cf2e3d7aa8be7c01
dwaipayanray95 <dwaipayanray95@gmail.com>
2026-08-20 14:22:13 +0530

    refactor: simplify fuel schematic card to single-line legend, no overlays

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,4 +1,4 @@
 class AppVersion {
-  static const String full = '3.3.2';
-  static const String display = 'v3.3.2';
+  static const String full = '3.3.3';
+  static const String display = 'v3.3.3';
 }
