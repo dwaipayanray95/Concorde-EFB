f2b19efc2be019cb579358afc23d42417f5e697f
dwaipayanray95 <dwaipayanray95@gmail.com>
2026-08-21 19:57:29 +0530

    fix: move airport DB cache off Documents on Windows

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,4 +1,4 @@
 class AppVersion {
-  static const String full = '3.4.5';
-  static const String display = 'v3.4.5';
+  static const String full = '3.4.6';
+  static const String display = 'v3.4.6';
 }
