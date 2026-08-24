bad1d22d6888d1312bf1c250c8e9dd140367503d
dwaipayanray95 <dwaipayanray95@gmail.com>
2026-08-24 17:35:30 +0530

    ci: rename release/beta installers to ConcordeEFB_vX.Y.Z(- beta_buildNN).exe

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,4 +1,4 @@
 class AppVersion {
-  static const String full = '3.4.13';
-  static const String display = 'v3.4.13';
+  static const String full = '3.4.14';
+  static const String display = 'v3.4.14';
 }
